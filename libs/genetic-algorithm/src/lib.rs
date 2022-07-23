f03a903c23434cd83a2df907267e5ffb8380bfd2
Chris Renfrow <dev@chrisrenfrow.me>
2022-07-23 13:41:28 -0700

    Add tests for roulette-style selection

--- libs/genetic-algorithm/src/lib.rs
+++ libs/genetic-algorithm/src/lib.rs
@@ -46,11 +46,60 @@ impl GeneticAlgorithm {
 }
 
 #[cfg(test)]
+#[derive(Clone, Debug)]
+pub struct TestIndividual {
+    fitness: f32,
+}
+
+#[cfg(test)]
+impl TestIndividual {
+    pub fn new(fitness: f32) -> Self {
+        Self { fitness }
+    }
+}
+
+#[cfg(test)]
+impl Individual for TestIndividual {
+    fn fitness(&self) -> f32 {
+        self.fitness
+    }
+}
+
+#[cfg(test)]
 mod tests {
     use super::*;
 
+    use rand::SeedableRng;
+    use rand_chacha::ChaCha8Rng;
+    use std::{collections::BTreeMap, iter::FromIterator};
+
     #[test]
     fn test() {
-        todo!();
+        let method = RouletteWheelSelection::new();
+        let mut rng = ChaCha8Rng::from_seed(Default::default());
+
+        let population = vec![
+            TestIndividual::new(2.0),
+            TestIndividual::new(1.0),
+            TestIndividual::new(4.0),
+            TestIndividual::new(3.0),
+        ];
+
+        let actual_histogram: BTreeMap<i32, _> = (0..1000)
+            .map(|_| method.select(&mut rng, &population))
+            .fold(Default::default(), |mut histogram, individual| {
+                *histogram.entry(individual.fitness() as _).or_default() += 1;
+
+                histogram
+            });
+
+        let expected_histogram = maplit::btreemap! {
+            1 => 98,
+            2 => 202,
+            3 => 278,
+            4 => 422
+        };
+
+        assert_eq!(actual_histogram, expected_histogram);
     }
 }
