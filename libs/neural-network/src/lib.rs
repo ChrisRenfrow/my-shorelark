974124c18b8936a67c4ece5858bd5b49701f76ee
Chris Renfrow <dev@chrisrenfrow.me>
2022-07-22 17:53:28 -0700

    Start implementing random initialization, non-idiomatically

--- libs/neural-network/src/lib.rs
+++ libs/neural-network/src/lib.rs
@@ -2,8 +2,27 @@ pub struct Network {
     layers: Vec<Layer>,
 }
 
+pub struct LayerTopology {
+    pub neurons: usize,
+}
+
 impl Network {
-    pub fn propagate(&self, mut inputs: Vec<f32>) -> Vec<f32> {
+    pub fn random(layers: Vec<LayerTopology>) -> Self {
+        let mut built_layers = Vec::new();
+
+        for i in 0..(layers.len() - 1) {
+            let input_neurons = layers[i].neurons;
+            let output_neurons = layers[i + 1].neurons;
+
+            built_layers.push(Layer::random(input_neurons, output_neurons));
+        }
+
+        Self {
+            layers: built_layers,
+        }
+    }
+
+    pub fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
         self.layers
             .iter()
             .fold(inputs, |inputs, layer| layer.propagate(inputs))
@@ -15,6 +34,10 @@ struct Layer {
 }
 
 impl Layer {
+    pub fn random(input_neurons: usize, output_neurons: usize) -> Self {
+        todo!()
+    }
+
     fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
         self.neurons
             .iter()
@@ -29,6 +52,10 @@ struct Neuron {
 }
 
 impl Neuron {
+    pub fn random(output_size: usize) -> Self {
+        todo!()
+    }
+
     fn propagate(&self, inputs: &[f32]) -> f32 {
         let output = inputs
             .iter()
