ff53cb9f6fab043321be4c544efcfd01f25177a8
Chris Renfrow <dev@chrisrenfrow.me>
2022-07-30 14:33:20 -0700

    Test mutation using TestIndividual

--- libs/genetic-algorithm/src/lib.rs
+++ libs/genetic-algorithm/src/lib.rs
@@ -69,12 +69,7 @@ where
         }
     }
 
-    pub fn evolve<I>(
-        &self,
-        rng: &mut dyn RngCore,
-        population: &[I],
-        evaluate_fitness: &dyn Fn(&I) -> f32,
-    ) -> Vec<I>
+    pub fn evolve<I>(&self, rng: &mut dyn RngCore, population: &[I]) -> Vec<I>
     where
         I: Individual,
     {
@@ -259,6 +254,7 @@ impl PartialEq for Chromosome {
 mod tests {
     use super::*;
 
+    use approx::assert_relative_eq;
     use rand::SeedableRng;
     use rand_chacha::ChaCha8Rng;
     use std::collections::BTreeMap;
@@ -291,6 +287,43 @@ mod tests {
         };
 
         assert_eq!(actual_histogram, expected_histogram);
+    }
+
+    fn individual(genes: &[f32]) -> TestIndividual {
+        let chromosome = genes.iter().cloned().collect();
+
+        TestIndividual::create(chromosome)
+    }
+
+    #[test]
+    fn test_mutation() {
+        let mut rng = ChaCha8Rng::from_seed(Default::default());
+
+        let ga = GeneticAlgorithm::new(
+            RouletteWheelSelection::new(),
+            UniformCrossover::new(),
+            GaussianMutation::new(0.5, 0.5),
+        );
+
+        let mut population = vec![
+            individual(&[0.0, 0.0, 0.0]), // fitness = 0.0
+            individual(&[1.0, 1.0, 1.0]), // fitness = 3.0
+            individual(&[1.0, 2.0, 1.0]), // fitness = 4.0
+            individual(&[1.0, 2.0, 4.0]), // fitness = 7.0
+        ];
+
+        for _ in 0..10 {
+            population = ga.evolve(&mut rng, &population);
+        }
+
+        let expected_population = vec![
+            individual(&[0.4476949, 2.0648358, 4.3058133]),
+            individual(&[1.2126867, 1.5538777, 2.886911]),
+            individual(&[1.0617678, 2.265739, 4.428764]),
+            individual(&[0.95909685, 2.4618788, 4.024733]),
+        ];
+
+        assert_eq!(population, expected_population);
     }
 
     #[test]
