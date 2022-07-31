a6cd01ffa9614d2e17941e31c43f692c954dc17b
Chris Renfrow <dev@chrisrenfrow.me>
2022-07-31 11:24:45 -0700

    Add random methods to all structs

--- libs/simulation/src/lib.rs
+++ libs/simulation/src/lib.rs
@@ -1,11 +1,15 @@
 use nalgebra as na;
+use rand::{Rng, RngCore};
 
 pub struct Simulation {
     world: World,
 }
 
 #[derive(Debug)]
-pub struct World;
+pub struct World {
+    animals: Vec<Animal>,
+    foods: Vec<Food>,
+}
 
 // TODO: Look into refactoring this using ECS (Entity Component System)
 #[derive(Debug)]
@@ -19,4 +23,39 @@ pub struct Animal {
 #[derive(Debug)]
 pub struct Food {
     position: na::Point2<f32>,
+}
+
+impl Simulation {
+    pub fn random(rng: &mut dyn RngCore) -> Self {
+        Self {
+            world: World::random(rng),
+        }
+    }
+}
+
+impl World {
+    fn random(rng: &mut dyn RngCore) -> Self {
+        let animals = (0..40).map(|_| Animal::random(rng)).collect();
+        let foods = (0..60).map(|_| Food::random(rng)).collect();
+
+        Self { animals, foods }
+    }
+}
+
+impl Animal {
+    pub fn random(rng: &mut dyn RngCore) -> Self {
+        Self {
+            position: rng.gen(),
+            rotation: rng.gen(),
+            speed: 0.002,
+        }
+    }
+}
+
+impl Food {
+    pub fn random(rng: &mut dyn RngCore) -> Self {
+        Self {
+            position: rng.gen(),
+        }
+    }
 }
