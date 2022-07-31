8672170f3dd49c60ede501307b1bdd63ef6a5266
Chris Renfrow <dev@chrisrenfrow.me>
2022-07-31 11:03:47 -0700

    Add basic structs to simulation

--- libs/simulation/src/lib.rs
+++ libs/simulation/src/lib.rs
@@ -1,1 +1,12 @@
-pub struct Simulation;
+pub struct Simulation {
+    world: World,
+}
+
+#[derive(Debug)]
+pub struct World;
+
+#[derive(Debug)]
+pub struct Animal;
+
+#[derive(Debug)]
+pub struct Food;
