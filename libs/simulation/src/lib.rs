6ef2c5b22ac6538a1606af578efb3724c5d6714c
Chris Renfrow <dev@chrisrenfrow.me>
2022-07-31 11:09:38 -0700

    Begin to specify fields for our structs using `nalgebra`

--- libs/simulation/src/lib.rs
+++ libs/simulation/src/lib.rs
@@ -1,3 +1,5 @@
+use nalgebra as na;
+
 pub struct Simulation {
     world: World,
 }
@@ -5,8 +7,16 @@ pub struct Simulation {
 #[derive(Debug)]
 pub struct World;
 
+// TODO: Look into refactoring this using ECS (Entity Component System)
 #[derive(Debug)]
-pub struct Animal;
+pub struct Animal {
+    position: na::Point2<f32>,
+    // The following could also be represented as a vector!
+    rotation: na::Rotation2<f32>,
+    speed: f32,
+}
 
 #[derive(Debug)]
-pub struct Food;
+pub struct Food {
+    position: na::Point2<f32>,
+}
