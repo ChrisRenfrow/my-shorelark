b2a023511635ba2b88fec1dde1fdbf604f0c3afd
Chris Renfrow <dev@chrisrenfrow.me>
2022-08-02 15:32:37 -0700

    Add from_weights methods and test

--- libs/neural-network/src/lib.rs
+++ libs/neural-network/src/lib.rs
@@ -10,6 +10,10 @@ pub struct LayerTopology {
 }
 
 impl Network {
+    fn new(layers: Vec<Layer>) -> Self {
+        Self { layers }
+    }
+
     pub fn random(rng: &mut dyn rand::RngCore, layers: &[LayerTopology]) -> Self {
         assert!(layers.len() > 1);
 
@@ -26,6 +30,34 @@ impl Network {
             .iter()
             .fold(inputs, |inputs, layer| layer.propagate(inputs))
     }
+
+    pub fn from_weights(layers: &[LayerTopology], weights: impl IntoIterator<Item = f32>) -> Self {
+        assert!(layers.len() > 1);
+
+        let mut weights = weights.into_iter();
+
+        let layers = layers
+            .windows(2)
+            .map(|layers| Layer::from_weights(layers[0].neurons, layers[1].neurons, &mut weights))
+            .collect();
+
+        if weights.next().is_some() {
+            panic!("got too many weights");
+        }
+
+        Self { layers }
+    }
+
+    pub fn weights(&self) -> Vec<f32> {
+        use std::iter::once;
+
+        self.layers
+            .iter()
+            .flat_map(|layer| layer.neurons.iter())
+            .flat_map(|neuron| once(&neuron.bias).chain(&neuron.weights))
+            .cloned()
+            .collect()
+    }
 }
 
 #[derive(Clone)]
@@ -34,6 +66,10 @@ struct Layer {
 }
 
 impl Layer {
+    pub fn new(neurons: Vec<Neuron>) -> Self {
+        Self { neurons }
+    }
+
     pub fn random(
         rng: &mut dyn rand::RngCore,
         input_neurons: usize,
@@ -41,6 +77,18 @@ impl Layer {
     ) -> Self {
         let neurons = (0..output_neurons)
             .map(|_| Neuron::random(rng, input_neurons))
+            .collect();
+
+        Self { neurons }
+    }
+
+    pub fn from_weights(
+        input_size: usize,
+        output_size: usize,
+        weights: &mut dyn Iterator<Item = f32>,
+    ) -> Self {
+        let neurons = (0..output_size)
+            .map(|_| Neuron::from_weights(input_size, weights))
             .collect();
 
         Self { neurons }
@@ -61,11 +109,25 @@ struct Neuron {
 }
 
 impl Neuron {
+    pub fn new(bias: f32, weights: Vec<f32>) -> Self {
+        Self { bias, weights }
+    }
+
     pub fn random(rng: &mut dyn rand::RngCore, output_size: usize) -> Self {
         let bias = rng.gen_range(-1.0..=1.0);
 
         let weights = (0..output_size)
             .map(|_| rng.gen_range(-1.0..=1.0))
+            .collect();
+
+        Self { bias, weights }
+    }
+
+    pub fn from_weights(output_neurons: usize, weights: &mut dyn Iterator<Item = f32>) -> Self {
+        let bias = weights.next().expect("got not enough weights");
+
+        let weights = (0..output_neurons)
+            .map(|_| weights.next().expect("got not enough weights"))
             .collect();
 
         Self { bias, weights }
@@ -130,6 +192,38 @@ mod tests {
             );
             assert_eq!(layer.propagate(vec![-0.3, 0.8]), vec![1.23, 1.23]);
             assert_eq!(network.propagate(vec![-0.3, 0.8]), vec![1.115, 1.115]);
+        }
+    }
+
+    mod weights {
+        use super::*;
+
+        #[test]
+        fn test() {
+            let network = Network::new(vec![
+                Layer::new(vec![Neuron::new(0.1, vec![0.2, 0.3, 0.4])]),
+                Layer::new(vec![Neuron::new(0.5, vec![0.6, 0.7, 0.8])]),
+            ]);
+
+            let actual = network.weights();
+            let expected = vec![0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8];
+
+            approx::assert_relative_eq!(actual.as_slice(), expected.as_slice(),);
+        }
+    }
+
+    mod from_weights {
+        use super::*;
+
+        #[test]
+        fn test() {
+            let layers = &[LayerTopology { neurons: 3 }, LayerTopology { neurons: 2 }];
+
+            let weights = vec![0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8];
+            let network = Network::from_weights(layers, weights.clone());
+            let actual: Vec<_> = network.weights().collect();
+
+            approx::assert_relative_eq!(actual.as_slice(), weights.as_slice(),);
         }
     }
 }
